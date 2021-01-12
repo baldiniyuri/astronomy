dc22cf56edaf7b4ea72b88950feea9ba4465d28a
Yuri Baldini <yuriabaldini@gmail.com>
2021-01-12 16:19:59 -0300

    fix: fix spelling in about pagr

--- src/Pages/About/about.jsx
+++ src/Pages/About/about.jsx
@@ -1,6 +1,6 @@
 import "../../Style/about.css";
 const About = () => {
-  return <div className="master-div">Under Constroction</div>;
+  return <div className="master-div">Under Construction</div>;
 };
 
 export default About;
